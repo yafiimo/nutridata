3085fc7448ea98ab0b70c91f596d204413aec066
yafiimo <mohammed.yafii@hotmail.co.uk>
2017-09-03 04:33:54 +0100

    set variables in server file to constants

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var routes = require('./backend/config/routes');
-var app = express();
-var PORT = process.env.PORT || 3000;
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const routes = require('./backend/config/routes');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.static('node_modules'));
 app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser. urlencoded({ extended: true }));
 app.use(routes);
 
 app.listen(PORT, () => {
